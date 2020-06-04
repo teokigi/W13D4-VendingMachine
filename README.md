debf029882ad6c5fc43e63781cd94c961bd65099
Teo <43215755+teokigi@users.noreply.github.com>
2020-06-04 11:40:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# W13D4-VendingMachine
+ PairedLab Vending Machine with polymorphs in Java
---
+++ W13D4-VendingMachine/src/main/java/Product/ProductType.java
@@ -0,0 +1,8 @@
+package Product;
+
+public enum ProductType {
+        COLA(1,1),
+        CRISPS(0.50,2),
+        SWEETS(0.65,3);
+
+}
